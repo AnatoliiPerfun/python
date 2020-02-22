9a2ea364d3a1cc9c6a8a713cb603558bb0673e09
Anatolii Perfun <54056693+AnatoliiPerfun@users.noreply.github.com>
2020-02-22 17:42:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,58 @@
+print("You`re on Island in a dark cave!")
+print("Try to survive until rescue arrives!")
+print("Rules: ")
+print("All available commands are in CAPITAL LETTERS.")
+print("Any other command wxits the program.")
+text = input("First LOOK around...")
+if text == "LOOK":
+    text = input("LEFT you hear rustling. RIGHT you see a light.")
+    if text == "LEFT":
+        print("A snake bites you! You are dead =(")
+    elif text == "RIGHT":
+        text = int(input("How many steps will you walk? "))
+        if text == 0:
+            print("You stay and miss the rescue plane =(")
+        elif text < 10:
+            text = input("You see BERRIES and LEAVES to eat:")
+            if text == "BERRIES":
+                text = input("EAT them or NOT?")
+                if text == "EAT":
+                    print("Should not have. They make you sick =(")
+                elif text == "NOT":
+                   text = input(
+                       "You spot a plane! make a FIRE or run to a CLEARING? ")
+                   if text == "FIRE":
+                       print("You took too long =(")
+                   elif text == "CLEARING":
+                       print("They see you. You`re saved!!!")
+                   else:
+                           print("Only type in commmads in CAPITAL LETTERS.")
+                else:
+                    print("Only type in commmads in CAPITAL LETTERS.")
+
+            elif text == "LEAVES":
+               text = input("Boil them with OCEAN water or PUDDLE water?")
+               if text == "OCEAN":
+                   print("A little solty, but ok. You stay hydrated and live!")
+               elif text == "PUDDLE":
+                   print("You fall ill, and miss the rescue plain!=(")
+               else:
+                   print("Only type in commmads in CAPITAL LETTERS.")
+            else:
+                print("Only type in commands in CAPITAL LETTERS!")
+
+        elif text >= 10:
+            print("You walked off a cliff =(")
+
+        else:
+            print("You didn`t follow the commands.")
+
+    else:
+        print("Only type in commands in CAPITAL LETTERS!")
+else:
+    print("Only type in commands in CAPITAL LETTERS!")
+
+
 
 u1 = 9
 u2 = 6
