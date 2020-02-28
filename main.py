7250a5556e205df0b60f8678e69b4da2f8bdb056
Anatolii Perfun <54056693+AnatoliiPerfun@users.noreply.github.com>
2020-02-28 18:06:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,66 @@
 
-def read_file(file):
+def check_player_win(board, token):
+     
+    row1 = (board[0][0] == board[0][1] == board[0][2] == token)
+    row2 = (board[1][0] == board[1][1] == board[1][2] == token)
+    row3 = (board[2][0] == board[2][1] == board[2][2] == token)
+    col1 = (board[0][0] == board[1][0] == board[2][0] == token)
+    col2 = (board[0][1] == board[1][1] == board[2][1] == token)
+    col3 = (board[0][2] == board[1][2] == board[2][2] == token)
+    diag1 = (board[0][0] == board[1][1] == board[2][2] == token)
+    diag2 = (board[2][0] == board[1][1] == board[0][2] == token)
+    return row1 or row2 or row3 or col1 or col2 or col3 or diag1 or diag2
+
+game = "xox|oox|xoo"
+board = game.split("|")
+
+winner_o = check_player_win(board, "o")
+winner_x = check_player_win(board, "x")
+ 
+if not winner_o and not winner_x:
+    print("it is a draw!")
+elif winner_o:
+    print("o is the winner!")    
+elif winner_x:
+    print("x is the winner!")
     
+print(board)
+
+
+
+
+
+x = "x"
+o = "o"
+empty = " "
+tictactoe = [[x, empty, o], [o,x,empty], [empty,o,x]]
+print(tictactoe)
+
+
+
+heights = [1,5,2,3,6,2.5,9,0.5,3]
+heights.sort()
+print(heights)
+heights.reverse()
+print(heights)
+
+
+
+grocery =[]
+grocery.append("COW")
+grocery.append("farm")
+grocery.insert(1, " living at the ")
+
+fun = [". Whole year long - happy cow."]
+grocery.extend(fun)
+grocery.pop(1)
+grocery[0] = "duck"
+print(grocery)
+
+
+
+
+def read_file(file):
     
     first_every_2 = ()
     second_every_2 = ()
